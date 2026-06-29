540d83f78fd43446b7439fd5d20c3825989a8d57
arajbanshi <ashish.rajbanshi@gmail.com>
2026-06-29 14:27:17 -0600

    agent loop

--- agent_search.py
+++ agent_search.py
@@ -1,37 +1,38 @@
+from typing import List
+from pydantic import BaseModel, Field
 from dotenv import load_dotenv
 from langchain.agents import create_agent
 from langchain_core.messages import HumanMessage
 from langchain.tools import tool
 from langchain_openai import ChatOpenAI
+from langchain_tavily import TavilySearch
 
 
 load_dotenv()
 
-@tool
-def search(query: str) -> str:
-    """
-    Searches the web for the given query and returns the results.
-    Args:
-        query (str): The search query.
-    Returns:
-        str: The search results.
-    """
-    print(f"Searching for: {query}")
-    return "Tokyo weather is sunny"
+class Source(BaseModel):
+    """Schema for a source used by the agent"""
+    url: str = Field(..., description="The URL of the source")
+
+class AgentResponse(BaseModel):
+    """Schema for the agent's response with answer and sources"""
+    answer: str = Field(..., description="The agent's answer to the query")
+    sources: List[Source] = Field(default_factory=list, description="A list of sources used to generate the answer")
 
 
 def main():
     print("Hello from agent search course!")
 
     llm = ChatOpenAI(model="gpt-5")
-    tools = [search]
+    tools = [TavilySearch()]
 
     agent = create_agent(
         model=llm,
-        tools=tools
+        tools=tools,
+        response_format=AgentResponse
     )
 
-    result = agent.invoke({"messages": [HumanMessage(content="What is the weather in Tokyo?")]})
+    result = agent.invoke({"messages": [HumanMessage(content="search for 3 job postings for an ai engineer using langchain in the bay area on linkedin and list their details?")]})
     print(result)
 
 
